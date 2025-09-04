3ccf14e9d09359d72270f202c8a6056359763592
Anirudh Nekkanti <nas@Anirudhs-MacBook-Pro.local>
2025-09-03 19:14:16 -0500

    Updated Floating Chat Button

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Sidebar from './components/Sidebar';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
+import { MessageCircle } from 'lucide-react';
 import './App.css';
 
 function App() {
@@ -14,6 +15,10 @@ function App() {
         <Header />
         <MainContent activeSection={activeSection} />
       </div>
+      {/* Floating Chat Button */}
+      <button className="floating-chat-btn">
+        <MessageCircle size={28} />
+      </button>
     </div>
   );
 }
